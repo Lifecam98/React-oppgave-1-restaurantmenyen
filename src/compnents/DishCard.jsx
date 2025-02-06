f851965545f9d0f2337d3b6c952d8d7dcbd84171
Lifecam98 <tma092024@gmail.com>
2025-02-06 13:43:48 +0100

    v1.1

--- src/compnents/DishCard.jsx
+++ src/compnents/DishCard.jsx
@@ -1,10 +1,10 @@
 const DishCard = ({ id, tittel, ingredienser, pris, kategori }) => {
 	return (
-		<div id="frame">
-			<h2>{tittel}</h2>
-			<p>{pris}</p>
-			<p>{ingredienser}</p>
-			<p>{kategori}</p>
+		<div id="frame" className="font-mono bg-blue-600 rounded-4xl p-4">
+			<h2 className="text-white font-extrabold">{tittel}</h2>
+			<p className="font-semibold">{pris}</p>
+			<p className="text-base/7">{ingredienser}</p>
+			<p className="italic leading-15">{kategori}</p>
 		</div>
 	);
 };
--- src/compnents/MenuLayout.jsx
+++ src/compnents/MenuLayout.jsx
@@ -4,7 +4,7 @@ import "../styles/menuLayout.css";
 const MenuLayout = ({ menu }) => {
 	return (
 		<>
-			<h1>Menu</h1>
+			<h1 className="text-4xl text-blue-600 font-extrabold">Menu</h1>
 			<ul>
 				{menu.map((obj) => (
 					<DishCard {...obj} key={obj.id} />

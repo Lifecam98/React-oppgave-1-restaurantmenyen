a0d3c8f3d5168d943c3164184d48ab17ca463ccb
Lifecam98 <tma092024@gmail.com>
2025-02-06 20:49:19 +0100

    v1.4

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import menu from "./data/menuItems.jsx";
 
 function App() {
 	const [selectedCategory, setSelectedCategory] = useState("Alle");
+	const [priceSort, setPriceSort] = useState("none")
 
 	const categories = ["Alle", "Forrett", "Hovedrett", "Dessert"];
 
-	const filteredMenu =
-		selectedCategory === "Alle"
-			? menu
-			: menu.filter((item) => item.kategori === selectedCategory);
+	const filteredMenu = menu
+		.filter(item => selectedCategory === "Alle" ? true : item.kategori === selectedCategory)
+		.sort((a, b) => {
+			if (priceSort === "ascending") {
+				return parseInt(a.pris) - parseInt(b.pris);
+			} else if (priceSort === "descending") {
+				return parseInt(b.pris) - parseInt(a.pris);
+			}
+			return 0;
+		});
+
 
 	return (
 		<>
@@ -19,6 +27,8 @@ function App() {
 				categories={categories}
 				selectedCategory={selectedCategory}
 				onCategoryChange={setSelectedCategory}
+				priceSort={priceSort}
+				onPriceSortChange={setPriceSort}
 			/>
 		</>
 	);
--- src/compnents/MenuLayout.jsx
+++ src/compnents/MenuLayout.jsx
@@ -6,11 +6,16 @@ const MenuLayout = ({
 	categories,
 	selectedCategory,
 	onCategoryChange,
+	priceSort,
+	onPriceSortChange,
 }) => {
 	return (
 		<>
 			<h1 className="text-5xl text-blue-600 font-extrabold">Menu</h1>
 			<div className="my-4">
+				<div className="mb-2">
+					<span className="text-blue-200">Sorter type rett:</span>
+				</div>
 				{categories.map((category) => (
 					<button
 						key={category}
@@ -24,6 +29,31 @@ const MenuLayout = ({
 						{category}
 					</button>
 				))}
+			</div>
+			<div className="my-4">
+				<div className="mb-2">
+					<span className="text-blue-200">Sorter etter pris:</span>
+				</div>
+								<button
+                    onClick={() => onPriceSortChange("none")}
+                    className={`mx-2 px-4 py-2 rounded-lg ${
+                        priceSort === "none"
+                            ? "bg-blue-600 text-blue-200"
+                            : "bg-blue-200 text-blue-800"
+                    }`}
+                >
+                    Ingen sortering
+                </button>
+				<button onClick={() => onPriceSortChange("ascending")}
+					className={`mx-2 px-4 py-2 rounded-lg ${priceSort === "ascending" ? "bg-blue-600 text-blue-200" : "bg-blue-200 text-blue-800"}`}
+				>
+					Stigende
+				</button>
+				<button onClick={() => onPriceSortChange("descending")}
+					className={`mx-2 px-4 py-2 rounded-lg ${priceSort === "descending" ? "bg-blue-600 text-blue-200" : "bg-blue-200 text-blue-800"}`}
+				>
+					Synkende
+				</button>
 			</div>
 			<ul>
 				{menu.map((obj) => (

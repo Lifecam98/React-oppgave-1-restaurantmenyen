6ef53bc1823daa34a164e8cc86426d208fcda206
Lifecam98 <tma092024@gmail.com>
2025-02-10 22:11:29 +0100

    v1.6

--- src/compnents/DishCard.jsx
+++ src/compnents/DishCard.jsx
@@ -1,6 +1,6 @@
 const DishCard = ({ id, tittel, ingredienser, pris, kategori }) => {
 	return (
-		<div className="font-mono bg-blue-600 rounded-4xl flex flex-col justify-between">
+		<div className="font-mono bg-blue-600 rounded-xl flex flex-col justify-between shadow-[0_0_20px_rgba(255,255,255,0.3)] hover:shadow-[0_0_30px_rgba(255,255,255,0.5)] transition-all duration-300 m-4">
 			<p>{id}</p>
 			<h2 className="text-blue-200 font-extrabold text-2xl">{tittel}</h2>
 			<p className="font-semibold">{pris}</p>

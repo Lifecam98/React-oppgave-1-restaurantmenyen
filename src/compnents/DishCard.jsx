45d6f4f4be5ee00266eb2403686e2341f79936c2
Lifecam98 <tma092024@gmail.com>
2025-02-10 13:17:23 +0100

    v1.5

--- src/compnents/DishCard.jsx
+++ src/compnents/DishCard.jsx
@@ -1,9 +1,11 @@
 const DishCard = ({ id, tittel, ingredienser, pris, kategori }) => {
 	return (
 		<div className="font-mono bg-blue-600 rounded-4xl flex flex-col justify-between">
+			<p>{id}</p>
 			<h2 className="text-blue-200 font-extrabold text-2xl">{tittel}</h2>
 			<p className="font-semibold">{pris}</p>
 			<p className="text-base/7">{ingredienser}</p>
+
 			<p className="italic leading-15">{kategori}</p>
 		</div>
 	);
